9d9c2a4732a5380df1eefb4bd12e7bdb803b063f
Joseph <JosephVolpe@outlook.com>
2022-09-22 00:12:50 -0400

    datasource updated for production

--- data-source.ts
+++ data-source.ts
@@ -6,7 +6,25 @@ import { Library } from "./entity/Library"
 import { Bookgenres } from "./entity/Bookgenres"
 import { Bookchapters } from "./entity/Bookchapters"
 
-export const AppDataSource = new DataSource({
+export let AppDataSource:DataSource;
+
+if (process.env.NODE_ENV == "production") {
+    AppDataSource =  new DataSource({
+        type: "postgres",
+        host: "ec2-44-198-24-0.compute-1.amazonaws.com",
+        port: 5432,
+        username: "yhslrfopruboqu",
+        password: "f8ee68219de833159b732325e940174f7f10bc0138daa17b009dcb745cc13b8a",
+        database: "d2qbqedi3pjns4",
+        synchronize: true,
+        logging: false,
+        entities: [User, Book, Library, Bookgenres, Bookchapters],
+        migrations: [],
+        subscribers: [],
+    })
+}
+else {
+    AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
@@ -19,3 +37,8 @@ export const AppDataSource = new DataSource({
     migrations: [],
     subscribers: [],
 })
+}
+
+
+ AppDataSource;
+
