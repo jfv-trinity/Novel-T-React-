a76a50dfe2c83c8db191a7138d5713a9db364f57
Joseph <JosephVolpe@outlook.com>
2022-09-23 13:50:31 -0400

    test

--- data-source.ts
+++ data-source.ts
@@ -15,12 +15,11 @@ if (process.env.NODE_ENV == "production") {
         username: "dfwgnjbmfclrjc",
         port: 5432,
         password: "b00ed334dbaa45d25bafdf14f25226ec58fe975d44abcca842a27e2c91e7bb65",
-        // synchronize: true,
+        synchronize: true,
         logging: false,
         entities: [User, Book, Library, Bookgenres, Bookchapters],
         migrations: [],
         subscribers: [],
-        url: "postgres://dfwgnjbmfclrjc:b00ed334dbaa45d25bafdf14f25226ec58fe975d44abcca842a27e2c91e7bb65@ec2-34-227-135-211.compute-1.amazonaws.com:5432/d7thqat7e1if1d"
     })
 }
 else {
@@ -38,6 +37,5 @@ else {
     migrations: [],
     subscribers: [],
     })
-    console.log(AppDataSource, "this is the app data source");
      console.log(process.env.NODE_ENV, ": this is the process env");
 }
