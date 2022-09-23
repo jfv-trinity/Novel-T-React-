c3ef0be568c2aa4000462d98680f537c439d7863
Joseph <JosephVolpe@outlook.com>
2022-09-23 12:49:28 -0400

    test

--- data-source.ts
+++ data-source.ts
@@ -8,14 +8,13 @@ import { Bookchapters } from "./entity/Bookchapters"
 export let AppDataSource:DataSource;
 
 if (process.env.NODE_ENV == "production") {
-    AppDataSource =  new DataSource({
+    AppDataSource = new DataSource({
         type: "postgres",
         host: "ec2-34-227-135-211.compute-1.amazonaws.com",
-        port: 5432,
-        username: "dfwgnjbmfclrjc",
-        password: "b00ed334dbaa45d25bafdf14f25226ec58fe975d44abcca842a27e2c91e7bb65",
         database: "d7thqat7e1if1d",
-        synchronize: true,
+        username: "dfwgnjbmfclrjc",
+        port: 5432,
+        password: "b00ed334dbaa45d25bafdf14f25226ec58fe975d44abcca842a27e2c91e7bb65",
         logging: false,
         entities: [User, Book, Library, Bookgenres, Bookchapters],
         migrations: [],
