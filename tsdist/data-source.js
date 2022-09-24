1fed2354920154dee7560588ac92dc0a5afa3767
Joseph <JosephVolpe@outlook.com>
2022-09-24 17:32:22 -0400

    test environment variable

---
+++ tsdist/data-source.js
@@ -0,0 +1,40 @@
+import { DataSource } from "typeorm";
+import { Book } from "./entity/Book";
+import { User } from "./entity/User";
+import { Library } from "./entity/Library";
+import { Bookgenres } from "./entity/Bookgenres";
+import { Bookchapters } from "./entity/Bookchapters";
+export var AppDataSource;
+console.log("this is the process env", process.env.NODE_ENV);
+if (process.env.NODE_ENV == "production") {
+    AppDataSource = new DataSource({
+        type: "postgres",
+        host: "ec2-34-227-135-211.compute-1.amazonaws.com",
+        database: "d7thqat7e1if1d",
+        username: "dfwgnjbmfclrjc",
+        port: 5432,
+        password: "b00ed334dbaa45d25bafdf14f25226ec58fe975d44abcca842a27e2c91e7bb65",
+        synchronize: true,
+        logging: false,
+        entities: [User, Book, Library, Bookgenres, Bookchapters],
+        migrations: [],
+        subscribers: [],
+        ssl: { rejectUnauthorized: false }
+    });
+}
+else {
+    AppDataSource = new DataSource({
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: "postgres",
+        password: "Abathur",
+        database: "react-novel-t",
+        synchronize: true,
+        logging: false,
+        entities: [User, Book, Library, Bookgenres, Bookchapters],
+        migrations: [],
+        subscribers: [],
+    });
+    console.log(process.env.NODE_ENV, ": this is the process env");
+}
---
+++ tsdist/entity/Bookgenres.js
@@ -0,0 +1,104 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Column, Entity, Index, PrimaryGeneratedColumn, } from "typeorm";
+var Bookgenres = /** @class */ (function () {
+    function Bookgenres() {
+    }
+    __decorate([
+        PrimaryGeneratedColumn({ type: "integer", name: "id" }),
+        __metadata("design:type", Number)
+    ], Bookgenres.prototype, "id", void 0);
+    __decorate([
+        Column("character varying", { name: "book_title", length: 150 }),
+        __metadata("design:type", String)
+    ], Bookgenres.prototype, "bookTitle", void 0);
+    __decorate([
+        Column("boolean", { name: "sci_fi", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "sciFi", void 0);
+    __decorate([
+        Column("boolean", { name: "fantasy", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "fantasy", void 0);
+    __decorate([
+        Column("boolean", { name: "romance", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "romance", void 0);
+    __decorate([
+        Column("boolean", { name: "action_adventure", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "actionAdventure", void 0);
+    __decorate([
+        Column("boolean", { name: "slice_of_life", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "sliceOfLife", void 0);
+    __decorate([
+        Column("boolean", { name: "comedy", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "comedy", void 0);
+    __decorate([
+        Column("boolean", { name: "tragedy", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "tragedy", void 0);
+    __decorate([
+        Column("boolean", { name: "mystery", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "mystery", void 0);
+    __decorate([
+        Column("boolean", { name: "thriller", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "thriller", void 0);
+    __decorate([
+        Column("boolean", { name: "horror", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "horror", void 0);
+    __decorate([
+        Column("boolean", { name: "isekai", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "isekai", void 0);
+    __decorate([
+        Column("boolean", { name: "reincarnation", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "reincarnation", void 0);
+    __decorate([
+        Column("boolean", { name: "transmigration", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "transmigration", void 0);
+    __decorate([
+        Column("boolean", { name: "historical", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "historical", void 0);
+    __decorate([
+        Column("boolean", { name: "military", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "military", void 0);
+    __decorate([
+        Column("boolean", { name: "school", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "school", void 0);
+    __decorate([
+        Column("boolean", { name: "spy", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "spy", void 0);
+    __decorate([
+        Column("boolean", { name: "martial_arts", nullable: true }),
+        __metadata("design:type", Object)
+    ], Bookgenres.prototype, "martialArts", void 0);
+    __decorate([
+        Column("integer", { name: "book_id" }),
+        __metadata("design:type", Number)
+    ], Bookgenres.prototype, "bookId", void 0);
+    Bookgenres = __decorate([
+        Index("bookgenres_pkey", ["id"], { unique: true }),
+        Entity("bookgenres", { schema: "public" })
+    ], Bookgenres);
+    return Bookgenres;
+}());
+export { Bookgenres };
