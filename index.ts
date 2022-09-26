a4d8a930bf5938fbeba066421fc5d9f63bf28c18
Joseph <JosephVolpe@outlook.com>
2022-09-26 15:17:22 -0400

    proxy added and * removed from routes

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ AppDataSource.initialize().then(async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 
-    app.get('/*', function (req: Request, res: Response) {
+    app.get('/', function (req: Request, res: Response) {
       res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
   }
