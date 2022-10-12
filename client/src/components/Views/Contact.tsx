fffe471c747c5ec784230b8aedccbd088a80db92
Joseph <JosephVolpe@outlook.com>
2022-10-12 19:40:29 -0400

    potential resume background fix for prod

--- client/src/components/Views/Contact.tsx
+++ client/src/components/Views/Contact.tsx
@@ -1,24 +1,17 @@
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import * as styles from "./Contact.scss";
+// import * as styles from "./Contact.scss";
+import pdf from "../../static/myResume.pdf";
+import background from "../../static/images/resume-background.jpg";
 
 const Contact = () => {
   return (
     <div>
       <Helmet>
-        <style>{`body { background-image: ${styles.default.background}; } `}</style>
+        {/* <style>{`body { background-image: ${background}; } `}</style> */}
+        <style>{`body { background-image: ${`url(${background});`} `}</style>
       </Helmet>
-      <div>
-        <embed
-          className="1"
-          src={styles.default.resume.slice(4, styles.default.resume.length - 1)}
-        />
-      </div>
-      <div>
-        <embed className="2" src={styles.default.resume} />
-      </div>
-      <div>
-        <img className="3" src={styles.default.srcBackground} />
-      </div>
+      <embed className="resume" src={pdf} />
     </div>
   );
 };
