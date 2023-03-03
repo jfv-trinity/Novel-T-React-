e71174726f134ebcf8ed7a9933c6b41e25edf580
jfv-trinity <Jvolpe7@my.centennialcollege.ca>
2023-03-03 01:32:46 -0500

    Repository Moved

--- data-source.ts
+++ data-source.ts
@@ -11,11 +11,11 @@ console.log("this is the process env", process.env.NODE_ENV);
 if (process.env.NODE_ENV == "production") {
     AppDataSource = new DataSource({
         type: "postgres",
-        host: "ec2-3-230-122-20.compute-1.amazonaws.com",
-        database: "d9d3n49njrgind",
-        username: "pdfhjvxklsvdzu",
+        host: "ec2-3-219-229-143.compute-1.amazonaws.com",
+        database: "d4ef6fs50ujdt9",
+        username: "zmymtehxdwbehj",
         port: 5432,
-        password: "0660688a2bf0acee127e5ef035ed7f30a84ed101ae0103f7df3fad413117aa09",
+        password: "2fc1a5a2c894548e34190f154bf992013077ee5806d1ae83595266ff6736a21c",
         synchronize: true,
         logging: false,
         entities: [User, Book, Library, Bookgenres, Bookchapters],
