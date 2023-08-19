f804482472162b0d41c5281340cf8c40f330122f
Updating volpe <Jvolpe7@my.centennialcollege.ca>
2023-08-18 21:00:10 -0400

    bookTitle added

--- client/src/common/Chapters.ts
+++ client/src/common/Chapters.ts
@@ -4,6 +4,7 @@ import UserProps from "./User";
 interface ChapterProps{
     id?: number;
     chapterTitle?: string;
+    bookTitle?: string;
     chapterNumber?: number;
     context?: string;
     bookId?: number;
