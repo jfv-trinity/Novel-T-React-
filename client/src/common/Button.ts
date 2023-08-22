b757db18ec428984dce99f302b0d1b29095981a6
Updating volpe <Jvolpe7@my.centennialcollege.ca>
2023-08-21 21:21:26 -0400

    props now addable

--- client/src/common/Button.ts
+++ client/src/common/Button.ts
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 
 interface ButtonProps{
+    [x: string]: any;
     id?: string;
     index?: number;
     onClick?:Dispatch<SetStateAction<boolean>>
